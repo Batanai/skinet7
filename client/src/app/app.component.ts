4173afe523a3b8ab086151ef14351832c6716198
Batanai <batiemc@gmail.com>
2023-07-31 21:08:44 +0200

    end of phase 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,31 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  title = 'Skinet';
-  products: Product[] = [];
+export class AppComponent implements OnInit {
 
-  constructor(private http: HttpClient){}
+  constructor() {}
 
-  ngOnInit(): void {
-      this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50')
-        .subscribe({
-          next: (response) => { 
-            console.log(response);
-            this.products = response.data;
-          }, //what to do next
-          error: (error) => console.log(error),
-          complete: () => {
-            console.log('process complete');
-            console.log('another statement');
-          }
-        })
-  }
+  ngOnInit(): void {}
 }
---
+++ client/src/app/shared/models/shopParams.ts
@@ -0,0 +1,8 @@
+export class SHopParams {
+    brandId = 0;
+    typeId = 0;
+    sort = 'name';
+    pageNumber = 1;
+    pageSize = 6;
+    search = '';
+}
