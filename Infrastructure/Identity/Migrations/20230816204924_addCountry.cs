4294709b7c67a990edec91e8e732576387842825
Batanai <batiemc@gmail.com>
2023-08-17 21:29:25 +0200

    end of phase 16

---
+++ Infrastructure/Identity/Migrations/20230816204924_addCountry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Address",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Address");
+        }
+    }
+}
